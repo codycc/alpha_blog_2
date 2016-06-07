4963249205ac38c1205643acb9f020137fe1a782
Cody Condon <codycondon50@gmail.com>
2016-06-06 22:44:41 -0400

    Create show action and add gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController <ApplicationController
     @user = User.new
   end
 
+  def create
+    @user =User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
   def edit
     @user = User.find(params[:id])
   end
@@ -15,17 +25,10 @@ class UsersController <ApplicationController
     else
       render 'edit'
     end
-
   end
 
-  def create
-    @user =User.new(user_params)
-    if @user.save
-      flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
-    else
-      render 'new'
-    end
+  def show
+    @user = User.find(params[:id])
   end
 
   private
