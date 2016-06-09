eb723a5a16b87f886fb24142471b61a90cb9f646
Cody Condon <codycondon50@gmail.com>
2016-06-09 12:54:58 -0400

    Add comments for list category test for clarity

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -8,8 +8,11 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test 'should show categories listing' do
+    # go to catergory path
     get categories_path
+    # check the template for index will be there
     assert_template 'categories/index'
+    # check that the link is to the category path and has the categories name
     assert_select 'a[href=?]', category_path(@category), text: @category.name
     assert_select 'a[href=?]', category_path(@category2), text: @category2.name
   end
